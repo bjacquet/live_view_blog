3b580a83312bb491f290520fe929ef8be88a5601
Bruno Jacquet <brunjact@protonmail.com>
2023-10-19 18:41:02 +0100

    Add LiveView component for post index

---
+++ lib/bruno_blog_web/live/post_live/index_component.ex
@@ -0,0 +1,19 @@
+defmodule BrunoBlogWeb.PostLive.IndexComponent do
+  use BrunoBlogWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <section id={"post_#{@post.id}"}>
+      <section>
+        <h3><%= live_redirect @post.title, to: Routes.post_show_path(@socket, :show, @post) %></h3>
+      </section>
+      <section>
+        <p><%= String.slice(@post.body, 0..80) %>…</p>
+      </section>
+      <section>
+        <p><%= @post.username %>, <%= @post.inserted_at %></p>
+      </section>
+    </section>
+    """
+  end
+end
